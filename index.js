52fa2d89d1354cecdbe0ad7495a22f8da7170d2c
Carl <narceil@hotmail.com>
2017-03-11 03:06:32 +1300

    Added basic tweet generation

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 
 require('dotenv').config();
 
-//var talisman = require('talisman');
-
 var twitter = require('./src/twitter');
+var Text = require('markov-chains-text').default;
 
-twitter.pullTweets().then(function(tweets){
+twitter.pullTweets(25).then(function(tweets){
+    var text = "";
     for (var i = 0; i < tweets.length; i++) {
-        console.log(tweets[i].text);
+        var str = tweets[i].text;
+
+        //Strip out the urls and newlines
+        str = str.replace(/http\S+/g, '');
+        str = str.replace('\n', ' ');
+
+        if (!text) { continue; }
+
+        text += str + '\n';
     }
+
+    console.log(text);
+
+    var chain = new Text(text);
+
+    for (var i = 0; i < 25; i++) {
+        var test = chain.makeSentence({
+            maxChars: 160
+        });
+        console.log(test);
+    }
+
 });
 //TODO: get tweets
 //TODO: analyze tweets
--- src/twitter.js
+++ src/twitter.js
@@ -1,58 +1,82 @@
-
 var Twitter = require('twitter');
 var client = new Twitter({
-	consumer_key: process.env.TWITTER_API_KEY,
-	consumer_secret: process.env.TWITTER_API_SECRET,
-	access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-	access_token_secret: process.env.TWITTER_ACCESS_SECRET
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_SECRET
 });
 
 module.exports = {
-	pullTweets: pullTweets,
-	postTweet: postTweet
+    pullTweets: pullTweets,
+    postTweet: postTweet
 };
 
 /**
-* Pull tweets down from twitter
-*/
-function pullTweets() {
-	return new Promise(function(resolve, reject) {
-        var tweets = [];
-        client.get('statuses/user_timeline', {
-            screen_name: 'meladoodle'
-        }, function(err, res){
-            if (err){
+ * Pull tweets down from twitter
+ */
+function pullTweets(loops) {
+    loops = loops || 5;
+
+    var requestBatch = function(num, lastTweet) {
+        return new Promise(function(resolve, reject) {
+            if (num >= loops) {
+                resolve([]);
+                return;
+            }
+
+            console.log("Requesting tweet batch "+(num+1));
+
+            var params = {
+                screen_name: 'meladoodle',
+                include_rts: 0,
+                exclude_replies: 1,
+                trim_user: 1,
+                count: 200
+            };
+
+            if (lastTweet) {
+                params.max_id = lastTweet.id;
+            }
+
+            getTweets(params).then(function(tweets) {
+                console.log("Received "+tweets.length+" items");
+                requestBatch(num + 1, tweets[tweets.length - 1]).then(function(res) {
+                    resolve(tweets.concat(res));
+                })
+            })
+        });
+    };
+
+    return new requestBatch(0, null);
+}
+
+function getTweets(params) {
+    return new Promise(function (resolve, reject) {
+
+        client.get('statuses/user_timeline', params, function (err, res) {
+            if (err) {
                 reject(err);
                 return;
             }
 
-            for (var i = 0; i < res.length; i++) {
-                var isRetweet = res[i].retweeted || res[i].text.match('^RT');
-                var isReply = !!res[i].in_reply_to_screen_name;
-
-                if (!isRetweet && !isReply) {
-                    tweets.push(res[i]);
-                }
-            }
-
-            resolve(tweets);
+            resolve(res);
         });
-	});
+    });
 }
 
 /**
-* Pull tweets down from twitter
-*/
+ * Pull tweets down from twitter
+ */
 function postTweet(status) {
-	client.post('statuses/update', {
-		status: status
-	}, function(err, tweet, res) {
-		if (!err) {
-			console.log(tweet);
-			console.log(res);
-		} else {
-			console.log(err);
-		}
-	});
+    client.post('statuses/update', {
+        status: status
+    }, function (err, tweet, res) {
+        if (!err) {
+            console.log(tweet);
+            console.log(res);
+        } else {
+            console.log(err);
+        }
+    });
 }
 
